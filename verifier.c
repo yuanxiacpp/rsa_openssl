5287efc08965dac0540c8a262807eb40e65d9541
Yuan Xia <yuan.xia.cpp@gmail.com>
2015-02-05 21:38:35 -0800

    update verifier.c

--- verifier.c
+++ verifier.c
@@ -3,10 +3,12 @@
 #include <openssl/err.h>
 #include <stdio.h>
 #include <string.h>
+#include <string>
 
 #define KEY_LENGTH  2048
 #define PUB_EXP     3
-
+int encrypt_len = 1;
+std::string pri_key, pub_key;
 bool generateRSAKeys() {
     RSA *keypair = RSA_generate_key(KEY_LENGTH, PUB_EXP, NULL, NULL);
 
@@ -20,18 +22,20 @@ bool generateRSAKeys() {
     size_t pri_len = BIO_pending(pri);
     size_t pub_len = BIO_pending(pub);
 
-    char* pri_key = (char*)malloc(pri_len + 1);
-    char* pub_key = (char*)malloc(pub_len + 1);
+    char* prikey = (char*)malloc(pri_len + 1);
+    char* pubkey = (char*)malloc(pub_len + 1);
 
-    BIO_read(pri, pri_key, pri_len);
-    BIO_read(pub, pub_key, pub_len);
+    BIO_read(pri, prikey, pri_len);
+    BIO_read(pub, pubkey, pub_len);
 
     pri_key[pri_len] = '\0';
     pub_key[pub_len] = '\0';
-    printf("\n%s\n%s\n", pri_key, pub_key);
+    printf("\n%s\n%s\n", prikey, pubkey);
+    pri_key(prikey);
+    pub_key(pubkey);
     return true;
 }
-bool encryptWithPub(char* pub_key, char* msg) {
+std::string encryptWithPub(char* msg) {
     char* encrypt;
     RSA *rsa = NULL;
     BIO* keybio = BIO_new_mem_buf(pub_key, -1);
@@ -50,15 +54,37 @@ bool encryptWithPub(char* pub_key, char* msg) {
         return "false";
     }
     printf("encrypted message: %s\n", encrypt);
-    return encrypt;
+    return std::string(encrypt);
 
 }
-char* decryptWtihPri(char* pri_key, char* msg);
+std::string decryptWtihPri(char* msg) {
+    char* decrypt = (char*)malloc(encrypt_len);
+    RSA *rsa = NULL;
+    BIO* keybio = BIO_new_mem_buf(pri_key.c_str(), -1);
+    if (keybio == NULL) {
+        printf("failed to create key BIO\n");
+        return "false";
+    }
+    rsa = PEM_read_bio_RSAPrivateKey(keybio, &rsa, NULL, NULL);
+    char *err = (char*)malloc(130);
+    if (RSA_private_decrypt(encrypt_len, (unsigned char*)msg, (unsigned char*) decrypt, rsa,
+                            RSA_PKCS1_OAEP_PADDING) == -1) {
+        ERR_load_crypto_strings();
+        ERR_error_string(ERR_get_error(), err);
+        fprintf(stderr, "Error decrypting message: %s\n", err);
+        return "false";
+    }
+    printf("Decrypted message: %s\n", decrypt);
+    return std::string(decrypt);
+}
 
-int main(void) {
-    char* pri_key, pub_key;
+int main() {
     if (!generateRSAKeys())
         printf("generateRSAKeys error\n");
+
+    std::string test("just for testing purpose");
+    decryptWtihPri(encryptWithPub(test.c_str()).c_str());
+
     
 /*
     // Get the message to encrypt
